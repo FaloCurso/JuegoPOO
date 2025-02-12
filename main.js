df2139e82d0787aa0146fb92de6513c5d53083c4
FaloCurso <falofr@gmail.com>
2025-02-12 08:19:55 +0100

    girar personaje

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-//clases
-
+// Clases
 class Game {
-    constructor(){
+    constructor() {
         this.container = document.getElementById("game-container");
         this.personaje = null;
         this.monedas = [];
@@ -11,106 +10,149 @@ class Game {
         this.crearEscenario();
         this.agregarEventos();
     }
-    crearEscenario(){   
-        this.personaje = new Personaje();
-        this.container.appendChild(this.personaje.element)  // lo posicionamos dentro del contenedor
-        for (let i = 0; i<5; i++){
-            const moneda = new Moneda();
+    
+    crearEscenario() {   
+        this.personaje = new Personaje(this.container); // Pasar el contenedor
+        this.container.appendChild(this.personaje.element);
+        
+        for (let i = 0; i < 5; i++) {
+            const moneda = new Moneda(this.container); // Pasar el contenedor
             this.monedas.push(moneda);
-            this.container.appendChild(moneda.element); //incluir la moneda dentro del contenedor definido en la linea 5
+            this.container.appendChild(moneda.element);
         }
     }
-    agregarEventos(){
-        window.addEventListener("keydown", (e) => this.personaje.mover(e)); // cuando se pulse una tecla presionada llama al personaje
-        this.chekColisiones();   // verificar si choca con una moneda
+    
+    agregarEventos() {
+        window.addEventListener("keydown", (e) => this.personaje.mover(e));
+        this.chekColisiones();
     }
-    chekColisiones(){
-        setInterval( ()=> {
-            this.monedas.forEach((moneda,index)=>{
-                if (this.personaje.colisionaCon(moneda)){ // si un personaje choca con una moneda que se quite del contenedor
+    
+    chekColisiones() {
+        setInterval(() => {
+            this.monedas.forEach((moneda, index) => {
+                if (this.personaje.colisionaCon(moneda)) {
                     this.container.removeChild(moneda.element);
-                    this.monedas.splice(index, 1); // quita la moneda edl array que representa el for por index
+                    this.monedas.splice(index, 1);
                 }
-            })
-        },
-            100) // se revisa cada milisegundo
+            });
+        }, 100);
     }
 }
-class Personaje{
-    constructor(){
+
+class Personaje {
+    constructor(container) {
+        this.container = container; // Guardar referencia al contenedor
         this.x = 50;
-        this.y = 300;
+        this.y = this.container.clientHeight - 60; // Ajustar para que no salga del contenedor
         this.width = 50;
-        this.height =50;
+        this.height = 50;
         this.velocidad = 10;
         this.saltando = false;
-        this.element = document.createElement("div"); // crea el div
+        this.ultimaDireccion = 1; // 1: derecha, -1: izquierda 0 caer
+        this.anchoContenedor = this.container.clientWidth; // Obtener el ancho real del contenedor
+        this.element = document.createElement("div");
         this.element.classList.add("personaje");
+        this.imagen = document.createElement("img");
+        this.imagen.src = "img/comecocod.png"; // Agregamos la imagen
+        this.element.appendChild(this.imagen);
         this.actualizarPosicion();
     }
-    mover(evento){
+    
+    mover(evento) {
         if (evento.key === "ArrowRight") { 
-             this.x += this.velocidad;
-         }else if(evento.key === "ArrowLeft" && this.x - this.velocidad >= 0){
+            if (this.x + this.width + this.velocidad <= this.anchoContenedor) {
+                this.x += this.velocidad;
+               if ( this.ultimaDireccion === -1){
+                   this.ultimaDireccion = 1;
+                   this.imagen.src = "img/comecocod.png"; 
+                   this.element.appendChild(this.imagen);
+               }
+                
+            }
+        } else if (evento.key === "ArrowLeft" && this.x - this.velocidad >= 0) {
             this.x -= this.velocidad;
-         }else if( evento.key === "ArrowUp"){
+            if ( this.ultimaDireccion === 1){
+                this.ultimaDireccion = -1;
+                this.imagen.src = "img/comecocoi.png"; 
+                this.element.appendChild(this.imagen);
+            }
+        } else if (evento.key === "ArrowUp" && !this.saltando) {
+            this.imagen.src = "img/comecocoa.png"; 
+            this.element.appendChild(this.imagen);
+            // this.ultimaDireccion = 0;
             this.saltar();
-         }
-         this.actualizarPosicion();
+        }
+        this.actualizarPosicion();
     }
-    saltar(){
+    
+    saltar() {
         this.saltando = true;
-        let alturaMaxima = this.y-100;
-        const salto = setInterval (() => {
-            if (this.y > alturaMaxima){
-                this.y -= 100; // se desplaza arriba gravedad 9.8
-            }else {
+        let alturaMaxima = this.y - 320;
+        const salto = setInterval(() => {
+            if (this.y > alturaMaxima) {
+                this.y -= 10;
+            } else {
                 clearInterval(salto);
                 this.caer();
             }
             this.actualizarPosicion();
-        },
-            20)
+        }, 20);
+        
     }
-    caer(){
+    
+    caer() {
         const gravedad = setInterval(() => {
-            if (this.y < 300 ){
+            if (this.y < this.container.clientHeight - 60) { 
                 this.y += 10;
-            }else {
+            } else {
                 clearInterval(gravedad);
+                this.saltando=false;
             }
             this.actualizarPosicion();
-        },
-            20)
+        }, 20);
+        if ( this.ultimaDireccion === -1){
+            this.imagen.src = "img/comecocoi.png"; 
+            this.element.appendChild(this.imagen);
+        }else  {
+            this.imagen.src = "img/comecocod.png"; 
+            this.element.appendChild(this.imagen);
+        }
     }
-    actualizarPosicion(){
+    
+    actualizarPosicion() {
         this.element.style.left = `${this.x}px`;
         this.element.style.top = `${this.y}px`;
     }
+    
     colisionaCon(objeto) { 
         return (
-          this.x < objeto.x + objeto.width &&
-          this.x + this.width > objeto.x &&
-          this.y < objeto.y + objeto.height &&
-          this.y + this.height > objeto.y
+            this.x < objeto.x + objeto.width &&
+            this.x + this.width > objeto.x &&
+            this.y < objeto.y + objeto.height &&
+            this.y + this.height > objeto.y
         );
     }
 }
-class Moneda{
-    constructor(){
-        this.x = Math.random()* 700 + 50;
-        this.y = Math.random()* 250 + 50;
+
+class Moneda {
+    constructor(container) {
+        this.container = container; // Guardar referencia al contenedor
+        this.x = Math.random() * (this.container.clientWidth - 40); 
+        this.y = Math.random() * (this.container.clientHeight - 40);
         this.width = 30;
         this.height = 30;
         this.element = document.createElement("div");
         this.element.classList.add("moneda");
         this.actualizarPosicion();
+        
+        console.log("Moneda creada en:", this.x, this.y);
     }
-    actualizarPosicion(){
+    
+    actualizarPosicion() {
         this.element.style.left = `${this.x}px`;
         this.element.style.top = `${this.y}px`;
     }
 }
-// instanciar
-const juego = new Game()
 
+// Instanciar el juego
+const juego = new Game();
